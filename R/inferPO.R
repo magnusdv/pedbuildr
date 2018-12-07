869d9a5770149e0acf87da5a8a5ea8046750f020
magnusdv <magnusdv@medisin.uio.no>
2018-12-07 09:57:57 +0100

    New function `inferPO()`

---
+++ R/inferPO.R
@@ -0,0 +1,24 @@
+# Estimate pairwise IBD coefficients and return
+# list of certain parent-child and *not* parent-child.
+#' @export
+inferPO = function(alleleMatrix, loci, list = FALSE) {
+  ids = rownames(alleleMatrix)
+  if(is.null(ids))
+    ids = idsnum = 1:nrow(alleleMatrix)
+
+  slist = lapply(ids, function(i)
+    setMarkers(singleton(i, sex=0), allele_matrix = alleleMatrix, locus_annotations = loci))
+
+  pairs = t.default(combn(ids, 2))
+  kappa = forrel::IBDestimate(slist, pairs)
+
+  PO = kappa[kappa$k0 < 0.01 & kappa$k2 < 0.5, , drop = F]
+  notPO = kappa[kappa$N != 0 & kappa$k0 > 0.5, , drop=F]
+
+  if(list) {
+    PO = lapply(seq_len(nrow(PO)), function(r) as.numeric(PO[r, 1:2]))
+    notPO = lapply(seq_len(nrow(notPO)), function(r) as.numeric(notPO[r, 1:2]))
+  }
+
+  list(PO = PO, notPO = notPO, kappa = kappa)
+}
