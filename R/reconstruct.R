07c41b96b6ecfa902874b9f959d16f1f0ee3464b
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2020-02-25 12:19:39 +0100

    S3 class and methods for output of `reconstruct()`

--- R/reconstruct.R
+++ R/reconstruct.R
@@ -2,73 +2,106 @@
 #'
 #' Reconstruct the most likely pedigree from genotype data
 #'
-#' @param alleleMatrix A matrix with two columns for each marker
-#' @param loci A list of marker attributes
+#' @param x A `ped` object or a list of such.
+#' @param ids A vector of ID labels from `x`. By default, the genotyped members of `x` are used.
+#' @param alleleMatrix A matrix with two columns for each marker.
+#' @param loci A list of marker attributes.
 #' @param pedlist A list of pedigrees. If NULL, [buildPeds()] is used to
-#'   generate a list
+#'   generate a list.
 #' @param pairwise A logical. If TRUE, an initial stage of pairwise IBD
 #'   estimation is done, in order to infer certain parent-child pairs, as well
 #'   as certain *non*-parent-child pairs. When this option is used, arguments to
-#'   `knownPO` and `notPO` are ignored
+#'   `knownPO` and `notPO` are ignored.
 #' @param founderInb A number in the interval `[0,1]`, used as background
 #'   inbreeding level in all founders
 #' @param sortResults A logical. If TRUE, the output is sorted so that the most
-#'   likely pedigree comes first
+#'   likely pedigree comes first.
 #' @param verbose A logical; verbose output or not
-#' @param ... Additional parameters passed on to [buildPeds()]
+#' @param ... Additional parameters passed on to [buildPeds()], e.g.,
+#'   `sex`, `knownPO`, `notPO`, `connected`, `maxLinearInbreeding`, `genderSym`.
 #'
-#' @return A list with two entries:
+#' @return An object of class `reconResult`, which is essentially list with the following entries:
 #'
-#'   * `pedlist` : A list of pedigrees. This equals (except possibly for
-#'   sorting) the input argument `pedlist` if this was given. If `sortResults =
+#'   * `pedlist` : A list of pedigrees, equal to the input argument `pedlist` if this was given. If `sortResults =
 #'   TRUE` then the list is sorted so that the most likely pedigrees come first
 #'
 #'   * `logliks` : A numerical vector containing the pedigree log-likelihoods
 #'
-#'   * `alleleMatrix` The input allele matrix
+#'   * `alleleMatrix` : The input allele matrix
+#'
+#'   * `labels` : A character vector of ID labels
 #'
 #' @examples
 #'
 #' # Simulate genotype data for a trio family (increase N!)
-#' x = forrel::markerSim(nuclearPed(1), N = 50, alleles = 1:3, seed = 123)
+#' x = forrel::markerSim(nuclearPed(1), N = 50, alleles = 1:3,
+#'                       seed = 123, verbose = FALSE)
 #'
-#' # Extract allele matrix and locus attributes (frequencies a.s.o.)
+#' # Extract marker data and reconstruct
+#' res = reconstruct(x, pairwise = TRUE)
+#'
+#' # Plot most likely pedigrees
+#' plot(res[1:6])
+#'
+#' # Alternative workflow: Extract data manually...
 #' m = getAlleles(x)
 #' loci = getLocusAttributes(x)
 #' sex = getSex(x)
 #'
-#' # Reconstruct the most likely pedigree from the data
-#' res = reconstruct(m, loci, sex = sex, connected = TRUE,
-#'                   genderSym = TRUE, pair=TRUE)
+#' # ...and then reconstruct
+#' res2 = reconstruct(alleleMatrix = m, loci = loci, sex = sex,
+#'                   pairwise = TRUE)
 #'
-#' # Plot the best pedigrees
-#' plotBestPeds(res)
+#' #
+#' y = forrel::markerSim(nuclearPed(children = c("s1", "s2")), ids = c("s1", "s2"),
+#'                       N = 50, alleles = 1:2, seed = 123, verbose = FALSE)
 #'
+#' # Reconstruct
+#' res3 = reconstruct(y, connected = FALSE)
+#' plot(res3)
 #'
 #' @importFrom utils setTxtProgressBar txtProgressBar
 #' @importFrom forrel IBDestimate showInTriangle
 #' @export
-reconstruct = function(alleleMatrix, loci, pedlist = NULL, pairwise = FALSE,
-                       founderInb = 0, sortResults = TRUE, verbose = TRUE, ...) {
-  ids = rownames(alleleMatrix)
-  if(is.null(ids))
-    ids = idsnum = 1:nrow(alleleMatrix)
-  else {
-    idsnum = suppressWarnings(as.integer(ids))
-    if(anyNA(idsnum))
-      stop2("Non-integer ID labels detected in `alleleMatrix`: ",
-            ids[is.na(idsnum)])
+reconstruct = function(x, ids, alleleMatrix = NULL, loci = NULL,
+                       pedlist = NULL, pairwise = FALSE, sortResults = TRUE,
+                       founderInb = 0, verbose = TRUE, ...) {
+  if(!missing(x)) {
+    if(!is.ped(x) || is.pedList(x))
+      stop2("Argument `x` must be a `ped` object, or a list of such")
+
+    if(missing(ids))
+      ids = typedMembers(x)
+
+    if(is.null(alleleMatrix))
+      alleleMatrix = getAlleles(x, ids)
+    else
+      stop2("When a ped/pedlist `x` is given as input, `alleleMatrix` must be NULL. (Data is extracted from `x`)")
+
+    loci = getLocusAttributes(x, markers = loci)
   }
-  if(verbose)
-    cat("ID labels inferred from allele matrix:", toString(ids[order(idsnum)]), "\n")
+  else {
+    ids = rownames(alleleMatrix)
+    if(is.null(ids))
+      ids = as.character(1:nrow(alleleMatrix))
+  }
 
+  # Change to numeric ids
+  idsnum = seq_along(ids)
+  rownames(alleleMatrix) = idsnum
+
+  ### Build pedigree list
   if(is.null(pedlist)) {
-    if(!identical(idsnum, 1:max(idsnum)))
-      stop2("\nFor now, only basic ID labels are supported. When `pedlist` is not supplied,\n",
-            "the rownames of `alleleMatrix` must be either NULL or 1,2,...,N")
-
     args = list(...)
 
+    # Sex: If `x` is given, extract from this
+    if(!missing(x)) {
+      if(!is.null(args$sex))
+        stop2("When a ped/pedlist `x` is given as input, `sex` must be NULL (it is extracted from `x`)")
+      args$sex = getSex(x, ids)
+    }
+
+    # Do pairwise analysis first?
     if(pairwise) {
       if(length(args$knownPO) > 0 | length(args$knownPO) > 0)
         stop2("`knownPO` and `notPO` must be NULL when `pairwise = TRUE`")
@@ -78,12 +111,10 @@ reconstruct = function(alleleMatrix, loci, pedlist = NULL, pairwise = FALSE,
 
       if(verbose) {
         forrel::showInTriangle(POresult$kappa, labels = TRUE, new = TRUE)
-        cat("Certain parent-child:\n")
-        print(POresult$PO, row.names = FALSE)
-        cat("\nCertain NON-parent-child:\n")
-        print(POresult$UN, row.names = FALSE)
-        cat("\n")
+        cat("Certain parent-child:", toString(sapply(POresult$PO, paste, collapse = "-")), "\n")
+        cat("Certain NON-parent-child:", toString(sapply(POresult$notPO, paste, collapse = "-")), "\n")
       }
+
       args$knownPO = POresult$PO
       args$notPO = POresult$notPO
     }
@@ -95,7 +126,7 @@ reconstruct = function(alleleMatrix, loci, pedlist = NULL, pairwise = FALSE,
 
   npeds = length(pedlist)
   if(npeds == 0)
-    return(NULL)
+    stop2("Empty pedigree list")
 
   if(verbose) {
     cat("\nComputing likelihoods of", npeds, "pedigrees\n")
@@ -140,7 +171,59 @@ reconstruct = function(alleleMatrix, loci, pedlist = NULL, pairwise = FALSE,
     logliks = logliks[ord]
   }
 
-  list(pedlist = pedlist, logliks = logliks, alleleMatrix = alleleMatrix)
+  structure(list(pedlist = pedlist,
+                 logliks = logliks,
+                 alleleMatrix = alleleMatrix,
+                 labels = ids),
+            class = "reconResult")
 }
 
+#' @export
+`[.reconResult` = function(x, i) {
+  structure(list(pedlist = x$pedlist[i],
+                 logliks = x$logliks[i],
+                 alleleMatrix = x$alleleMatrix,
+                 labels = x$labels),
+            class = "reconResult")
+}
 
+#' @importFrom graphics par plot text title
+#' @export
+plot.reconResult = function(x, titles = "logliks", nrow = NA, id.labels = x$labels,
+                            shaded = x$labels, col = list(red = x$labels), ...) {
+
+  L = length(x$pedlist)
+  if(is.na(nrow))
+    nrow = if(L<6) 1 else floor(sqrt(L))
+
+  op = par(mfrow = c(nrow, ceiling(L/nrow)))
+  on.exit(par(op))
+
+  if(identical(titles, "logliks"))
+    titles = paste("Loglik =", round(x$logliks, 2))
+
+  labs = x$labels
+
+  for(i in seq_along(x$pedlist)) {
+    ped = x$pedlist[[i]]
+
+    # Title
+    tit = if(!is.null(titles)) titles[i] else NULL
+
+    # Margin
+    mar = if(is.null(tit)) c(1.5,1.5,1.5,1.5) else c(1.5,1.5,3,1.5)
+
+    if(is.pedList(ped)) {
+      plot(-1:1,-1:1, type="n", axes = F, xlab="", ylab="")
+      mess ="Disconnected pedigree.\n\nPlot separately with\n`pedtools::plotPedList()`."
+      text(0, 0, mess, cex = 1.3)
+      title(tit, line= -2)
+      next
+    }
+
+    ped = relabel(ped, old = seq_along(labs), labs)
+
+    plot(ped, id.label = id.labels, shaded = shaded, col = col,
+         margin = mar, title = tit, ...)
+  }
+}
