3a394160ea0788038780630334bcda75929a2acd
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2021-04-28 16:30:38 +0200

    Clean-ups

--- R/likelihood.R
+++ R/likelihood.R
@@ -8,8 +8,6 @@ loglikTotal = function(x) {
   if(is.null(x))
     return(NA_real_)
 
-  #logliks = vapply(seq_len(nMark),
-  #   function(i) likelihood(x, i, logbase = exp(1)), FUN.VALUE = 1)
   logliks = likelihood(x, logbase = exp(1))
 
   sum(logliks)
