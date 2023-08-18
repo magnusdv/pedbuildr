c83c86d9738690d99e5175b1bfd3d6d340f601e1
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-08-18 10:47:50 +0200

    Generate "pedbuildr-package.R" with `usethis::use_package_doc()`

--- R/pedbuildr-package.R
+++ R/pedbuildr-package.R
@@ -1,14 +1,7 @@
-#' pedbuildr: Pedigree reconstruction
-#'
-#' Reconstruct pedigrees from genotype data, by optimising the likelihood over
-#' all possible pedigrees subject to given restrictions. Tailor-made plots
-#' facilitate evaluation of the output. This package is part of the 'ped suite'
-#' ecosystem for pedigree analysis in R. In particular, it imports 'pedprobr'
-#' for calculating pedigree likelihoods and 'forrel' for estimating pairwise
-#' relatedness.
-#'
-#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import pedtools
-#'
-#' @name pedbuildr
+## usethis namespace: end
 NULL
