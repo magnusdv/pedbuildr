ddf4e38388b69a82a4e5536ce9fb05cb74cfbd98
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-08-18 12:54:40 +0200

    Document dataset `Tutankhamun`

--- R/data.R
+++ R/data.R
@@ -3,17 +3,32 @@
 #' This dataset contains simulated genotypes for 3 males at 100 SNP markers.
 #'
 #' @format A data frame with 3 rows and 104 columns. The first 4 columns contain
-#'   pedigree info:
+#'   pedigree info in standard format:
 #'
 #'   * `id`: Individual ID
-#'
 #'   * `fid`: Paternal ID, where 0 means missing father
-#'
 #'   * `mid`: Maternal ID, where 0 means missing mother
-#'
 #'   * `sex`: 1 = male; 2 = female
 #'
 #'   The remaining 100 columns contain the genotypes, in the form a/b, where a
 #'   and b are the observed alleles (labelled 1 and 2).
 #'
 "trioData"
+
+
+#' Pedigree of Tutankhamun
+#'
+#' A reconstructed pedigree of the Egyptian Pharaoh Tutankhamun, with genotypes
+#' for 8 STR markers, as published by Hawass et al.
+#'
+#' @format A data frame with 7 rows and 12 columns:
+#'
+#'   * `id`,`fid`,`mid`,`sex`: Pedigree columns in standard format
+#'   * `D13S317`, ...: Genotype columns for 8 markers
+#'
+#'   See help page for [trioData] for further details on format.
+#'
+#' @source Hawass et al. *Ancestry and pathology in King Tutankhamun's family*.
+#'   Jama (2010).
+#'
+"Tutankhamun"
--- data-raw/Tutankhamun.R
+++ data-raw/Tutankhamun.R
@@ -1,16 +1,18 @@
 ## code to prepare `Tutankhamun` dataset goes here
 
-library(pedtools)
-Tutankhamun = readPed("data-raw/tutankhamun-jama.txt")
-
+library(pedsuite)
+Tutankhamun = readPed("data-raw/tutankhamun-jama.txt", colSep = "\t")
+plot(Tutankhamun)
 usethis::use_data(Tutankhamun, overwrite = TRUE)
 
+
 #### Testing
+showInTriangle(ibdEstimate(Tutankhamun)[11:6,], labels = T)
 
-showInTriangle(forrel::ibdEstimate(Tutankhamun)[11:6,], labels = T)
-
-t1 = reconstruct(Tutankhamun, ids = labels(Tutankhamun)[1:4], noChildren = labels(Tutankhamun)[3:4], linear = FALSE, extra = 2)
+t1 = reconstruct(Tutankhamun, ids = labels(Tutankhamun)[1:4],
+                 noChildren = labels(Tutankhamun)[3:4], linear = FALSE, extra = 2)
 plot(t1, 6)
+
 # Try NorwFreq
 q = 0.2; p = 1 - q
 db = forrel::NorwegianFrequencies
